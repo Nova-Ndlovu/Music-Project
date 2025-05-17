cb5c84dcb199d580d1df0f4b2cb741d4be48aa38
Phi Ndlovu <ndlovusiphilangenkosi@gmail.com>
2025-05-17 05:45:28 +0200

    Created the skeletal structure of the project
    Created the html, css and js files and linked them all together for the project. So glad to finally get started with it!

---
+++ source/script.js
@@ -0,0 +1,2 @@
+noticeElement = document.querySelector("#welcome-message");
+noticeElement.innerHTML = `<div>⚠⚠⚠</div> <div>Hello</div> <div>This page is still under construction.</div> <div>Please visit again later to witness the exciting developments!</div> <div>⚠⚠⚠</div>`;
